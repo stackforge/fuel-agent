681bc9ebfd671fa31853697fded656524133e312
Vladyslav Drok <vdrok@mirantis.com>
2015-07-14 19:37:17 +0300

    [Ironic] Ironic deploy support for Fuel Agent
    This patch adds support for Ironic Fuel Agent deploy
    driver: https://review.fuel-infra.org/#/c/9097/
    
    Co-Authored-By: Yuriy Zveryanskyy <yzveryanskyy@mirantis.com>
    Implements blueprint: baremetal-deploy-ironic
    
    Change-Id: Ia64099b0f99ca1844421e569fad2415c1214713f

--- fuel_agent/drivers/nailgun.py
+++ fuel_agent/drivers/nailgun.py
@@ -16,6 +16,7 @@ import itertools
 import math
 import os
 
+from oslo.config import cfg
 import six
 from six.moves.urllib.parse import urljoin
 from six.moves.urllib.parse import urlparse
@@ -32,6 +33,9 @@ from fuel_agent.utils import utils
 
 
 LOG = logging.getLogger(__name__)
+
+CONF = cfg.CONF
+CONF.import_opt('config_drive_path', 'fuel_agent.manager')
 
 
 def match_device(hu_disk, ks_disk):
@@ -225,6 +229,9 @@ class Nailgun(BaseDataDriver):
         os_release = self._image_meta.get('os', None)
         return self.get_os_by_image_meta(os_release) or \
             self.get_os_by_profile(self.data['profile'].lower())
+
+    def is_configdrive_needed(self):
+        return True
 
     def parse_partition_scheme(self):
         LOG.debug('--- Preparing partition scheme ---')
@@ -422,7 +429,8 @@ class Nailgun(BaseDataDriver):
                         self._boot_done = True
 
             # this partition will be used to put there configdrive image
-            if partition_scheme.configdrive_device() is None:
+            if (partition_scheme.configdrive_device() is None and
+                    self.is_configdrive_needed()):
                 LOG.debug('Adding configdrive partition on disk %s: size=20' %
                           disk['name'])
                 parted.add_partition(size=20, configdrive=True)
@@ -589,6 +597,22 @@ class Nailgun(BaseDataDriver):
                 md5=imeta.get('raw_md5'),
             )
         return image_scheme
+
+
+class Ironic(Nailgun):
+    def __init__(self, data):
+        super(Ironic, self).__init__(data)
+
+    def is_configdrive_needed(self):
+        return os.path.isfile(CONF.config_drive_path)
+
+    def parse_configdrive_scheme(self):
+        pass
+
+    def parse_partition_scheme(self):
+        self._boot_partition_done = True
+        self._boot_done = True
+        return super(Ironic, self).parse_partition_scheme()
 
 
 class NailgunBuildImage(BaseDataDriver):
