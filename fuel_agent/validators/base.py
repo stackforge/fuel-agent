a182a48f25817cf1e705bec457ece25fcc7c0779
Sebastian Kalinowski <sebastian@kalinowski.eu>
2015-07-15 14:24:37 +0200

    [WIP] More validators
    Change-Id: I28e3ffe053c975f37323c1e43bcbe357eeeecf1a
    Co-Authored-By: Sebastian Kalinowski <skalinowski@mirantis.com>
    Co-Authored-By: ...

--- fuel_agent/validators/base.py
+++ fuel_agent/validators/base.py
@@ -1,6 +1,14 @@
 import abc
 
+import six
 
+from fuel_agent import errors
+from fuel_agent.openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+@six.add_metaclass(abc.ABCMeta)
 class BaseValidator(object):
 
     @abc.abstractmethod
@@ -9,6 +17,12 @@ class BaseValidator(object):
 
     def __init__(self, *args, **kwargs):
         self.validate(*args, **kwargs)
+
+    @classmethod
+    def raise_error(self, message):
+        error = errors.WrongPartitionSchemeError(message)
+        LOG.exception(error)
+        raise error
 
 
 class DummyValidator(object):
