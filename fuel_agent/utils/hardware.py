8d685684e97b1a0acf98f1989f78bec39fc09c39
Andrey Tykhonov <atykhonov@mirantis.com>
2015-09-28 18:39:44 +0300

    Check whether given filename is a special device
    Accept only a special device file, otherwise throw an exception.
    
    Change-Id: I23598b52d273091a877896968d17377373322cae
    Closes-bug: #1492989

--- fuel_agent/utils/hardware.py
+++ fuel_agent/utils/hardware.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import os
+import stat
 
 from fuel_agent import errors
 from fuel_agent.openstack.common import log as logging
@@ -243,6 +244,12 @@ def extrareport(dev):
             pass
 
     return spec
+
+
+def is_special_device_file(self, filepath):
+    """Check whether `filepath` is block device or character device."""
+    mode = os.stat(filepath).st_mode
+    return stat.S_ISBLK(mode) or stat.S_ISCHR(mode)
 
 
 def get_block_devices_from_udev_db():
