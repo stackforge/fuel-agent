047f386f985f75e1bbfc2dcc3c3b3c165cd20473
Dmitry Guryanov <dguryanov@mirantis.com>
2016-04-06 21:01:16 +0300

    [RFC] change configdrive format to ConfigDrive version 2
    We put configdrive with is9660 filesystem to a
    partition on a hard disk. New hard disks may have
    4K sectors, but blocksize of iso9660 fs is 2K so
    it will not work.
    
    To fix this bug we should use another filesystem (ext2)
    and another config drive format (files, directory
    structure), because NoCloud format, which is currently
    used support only vfat and iso9660 filesystems.
    
    Note for reviewers: please, check only general idea,
    I'll write unit tests later.
    
    Change-Id: Ia0f244f19bab3dfaceef8a092ad03667675a5557
    Closes-Bug: #1544818

--- fuel_agent/manager.py
+++ fuel_agent/manager.py
@@ -16,6 +16,7 @@ from io import open
 import os
 import shutil
 import signal
+import tempfile
 
 from oslo_config import cfg
 import six
@@ -306,43 +307,104 @@ class Manager(object):
             if not found_images:
                 fu.make_fs(fs.type, fs.options, fs.label, fs.device)
 
+    def _get_required_fs_size(self, src_files):
+        def dir_size(d):
+            s = 0
+            for root, dirs, files in os.walk(d):
+                s += sum(os.path.getsize(os.path.join(root, name))
+                         for name in files + dirs)
+            return s
+
+        req_size = 0
+        for f in src_files:
+            req_size += os.path.getsize(f)
+            if os.path.isdir(f):
+                req_size += dir_size(f)
+
+        # 10% of size for fs metadata
+        req_size = int(req_size * 1.1)
+        # round up to megabytes
+        return (req_size - 1) / (1 << 20) + 1
+
+    def _make_configdrive_image(self, src_files):
+        req_size = self._get_required_fs_size(src_files)
+
+        utils.execute('dd', 'if=/dev/zero', 'bs=1M', 'count=%d' % req_size,
+                      'of=%s' % CONF.config_drive_path)
+        fu.make_fs(
+            fs_type='ext4',
+            fs_options=' -b 4096 -F ',
+            fs_label='config-2',
+            dev=six.text_type(CONF.config_drive_path))
+
+        mount_point = tempfile.mkdtemp(dir=CONF.tmp_path)
+        try:
+            fu.mount_fs('ext4', CONF.config_drive_path, mount_point)
+            for file_path in src_files:
+                name = os.path.basename(file_path)
+                if os.path.isdir(file_path):
+                    shutil.copytree(file_path, os.path.join(mount_point, name))
+                else:
+                    shutil.copy2(file_path, mount_point)
+        except Exception as exc:
+            LOG.error('Error copying files to configdrive: %s', exc)
+            raise
+        finally:
+            fu.umount_fs(mount_point)
+            os.rmdir(mount_point)
+
+    def _prepare_configdrive_files(self):
+        # see data sources part of cloud-init documentation
+        # for directory structure
+        cd_root = tempfile.mkdtemp(dir=CONF.tmp_path)
+        cd_latest = os.path.join(cd_root, 'openstack', 'latest')
+        cd_content = os.path.join(cd_root, 'openstack', 'content')
+        md_output_path = os.path.join(cd_latest, 'meta_data.json')
+        ud_output_path = os.path.join(cd_latest, 'user_data')
+        nc_output_path = os.path.join(cd_content, '0000')
+        os.makedirs(cd_latest)
+        os.makedirs(cd_content)
+
+        cc_output_path = os.path.join(CONF.tmp_path, 'cloud_config.txt')
+        bh_output_path = os.path.join(CONF.tmp_path, 'boothook.txt')
+
+        tmpl_dir = CONF.nc_template_path
+        utils.render_and_save(
+            tmpl_dir,
+            self.driver.configdrive_scheme.template_names('cloud_config'),
+            self.driver.configdrive_scheme.template_data(),
+            cc_output_path
+        )
+        utils.render_and_save(
+            tmpl_dir,
+            self.driver.configdrive_scheme.template_names('boothook'),
+            self.driver.configdrive_scheme.template_data(),
+            bh_output_path
+        )
+        utils.render_and_save(
+            tmpl_dir,
+            self.driver.configdrive_scheme.template_names('meta_data_json'),
+            self.driver.configdrive_scheme.template_data(),
+            md_output_path
+        )
+        utils.render_and_save(
+            tmpl_dir,
+            self.driver.configdrive_scheme.template_names('network_config'),
+            self.driver.configdrive_scheme.template_data(),
+            nc_output_path
+        )
+
+        utils.execute(
+            'write-mime-multipart', '--output=%s' % ud_output_path,
+            '%s:text/cloud-boothook' % bh_output_path,
+            '%s:text/cloud-config' % cc_output_path)
+        return [os.path.join(cd_root, 'openstack')]
+
     def do_configdrive(self):
         LOG.debug('--- Creating configdrive (do_configdrive) ---')
         if CONF.prepare_configdrive:
-            cc_output_path = os.path.join(CONF.tmp_path, 'cloud_config.txt')
-            bh_output_path = os.path.join(CONF.tmp_path, 'boothook.txt')
-            # NOTE:file should be strictly named as 'user-data'
-            #      the same is for meta-data as well
-            ud_output_path = os.path.join(CONF.tmp_path, 'user-data')
-            md_output_path = os.path.join(CONF.tmp_path, 'meta-data')
-
-            tmpl_dir = CONF.nc_template_path
-            utils.render_and_save(
-                tmpl_dir,
-                self.driver.configdrive_scheme.template_names('cloud_config'),
-                self.driver.configdrive_scheme.template_data(),
-                cc_output_path
-            )
-            utils.render_and_save(
-                tmpl_dir,
-                self.driver.configdrive_scheme.template_names('boothook'),
-                self.driver.configdrive_scheme.template_data(),
-                bh_output_path
-            )
-            utils.render_and_save(
-                tmpl_dir,
-                self.driver.configdrive_scheme.template_names('meta_data'),
-                self.driver.configdrive_scheme.template_data(),
-                md_output_path
-            )
-
-            utils.execute(
-                'write-mime-multipart', '--output=%s' % ud_output_path,
-                '%s:text/cloud-boothook' % bh_output_path,
-                '%s:text/cloud-config' % cc_output_path)
-            utils.execute('genisoimage', '-output', CONF.config_drive_path,
-                          '-volid', 'cidata', '-joliet', '-rock',
-                          ud_output_path, md_output_path)
+            files = self._prepare_configdrive_files()
+            self._make_configdrive_image(files)
 
         if CONF.prepare_configdrive or os.path.isfile(CONF.config_drive_path):
             self._add_configdrive_image()
@@ -358,7 +420,7 @@ class Manager(object):
         self.driver.image_scheme.add_image(
             uri='file://%s' % CONF.config_drive_path,
             target_device=configdrive_device,
-            format='iso9660',
+            format='ext4',
             container='raw',
             size=size,
             md5=md5,
