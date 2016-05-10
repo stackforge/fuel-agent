9d10292cd16c48bb6e86c093529a68cc62de0f46
Artur Svechnikov <asvechnikov@mirantis.com>
2016-05-10 16:35:04 +0300

    Basic configurations for fuel-bootstrap tests
    * Add tox.ini
    * Change run_tests.sh to run both fuel-agent tests
      and fuel-bootstrap tests
    * Add basic requirements test
    
    Change-Id: I877696503d56a511bd84ffe36099e6c57587e748
    Partial-Bug: #1522353

--- run_tests.sh
+++ run_tests.sh
@@ -15,6 +15,60 @@
 #    under the License.
 
 set -e
-set -x
 
-tox -v
+ROOT=$(dirname `readlink -f $0`)
+
+function usage {
+  echo "Usage: $0 [OPTION]..."
+  echo "Run Fuel-Agent test suite(s)"
+  echo ""
+  echo "  -h, --help                  Print this usage message"
+  echo "  -A, --no-agent              Skip Fuel Agent tests"
+  echo "  -B, --no-bootstrap          Skip Fuel Bootstrap tests"
+  echo ""
+  echo "Note: with no options specified, the script will try to run all available"
+  echo "      tests with all available checks."
+  exit
+}
+
+function process_options {
+  for arg in $@; do
+    case "$arg" in
+      -h|--help) usage;;
+      -A|--no-agent) agent_tests=0;;
+      -B|--no-bootstrap) bootstrap_tests=0
+    esac
+  done
+}
+
+agent_tests=1
+bootstrap_tests=1
+
+function run_cleanup {
+  find . -type f -name "*.pyc" -delete
+}
+
+function run_tests {
+    run_cleanup
+
+    if [ $agent_tests -eq 1 ]; then
+        run_agent_tests
+    fi
+
+    if [ $bootstrap_tests -eq 1 ]; then
+        run_bootstrap_tests
+    fi
+}
+
+function run_agent_tests {
+    tox -v
+}
+
+function run_bootstrap_tests {
+    pushd "$ROOT/contrib/fuel_bootstrap/fuel_bootstrap_cli" >> /dev/null
+    tox -v
+    popd >> /dev/null
+}
+
+process_options $@
+run_tests
